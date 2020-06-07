d36e22dfeafc1b1ed333c50a2744581e78394b68
Manuel <manuel-serret@t-online.de>
2020-06-07 09:18:54 +0200

    switch to express

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var live = express.static('public');
+
+app.use(live);
+app.get('/*', function (req, res) {
+    res.sendFile("index.html", {root: "public"});
+})
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
