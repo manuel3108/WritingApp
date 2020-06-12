351756f11c21bf7bd75b97a30b215d8a692c30d5
Manuel <manuel-serret@t-online.de>
2020-06-12 20:16:49 +0200

    first websocket

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const enableWs = require('express-ws')
 
 var live = express.static('public');
+enableWs(app)
 
+app.ws('/echo', (ws, req) => {
+    ws.on('message', msg => {
+        console.log("recieved message", msg)
+        ws.send(msg)
+    })
+
+    ws.on('close', () => {
+        console.log('WebSocket was closed')
+    })
+})
 app.use(live);
 app.get('/*', function (req, res) {
     res.sendFile("index.html", {root: "public"});
 })
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+
